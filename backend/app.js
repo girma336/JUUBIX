d0611193870491b825c19e433a46b55cd8be0282
girma <girmatarekegn.gi@gmail.com>
2023-07-28 20:56:27 +0300

    Add app.js file

---
+++ backend/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const colors = require('colors')
+const {connectDb} = require('./config/db')
+const { errorHandler } = require('./middelware/errorMiddelware')
+const dotenv = require("dotenv").config()
+const port =  process.env.PORT || 5000;
+
+const app = express();
+
+
+connectDb()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use('/api/tasks', require('./routes/taskRoutes'))
+app.use(errorHandler)
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
