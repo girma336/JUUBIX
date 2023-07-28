43e6653321508f0b97d612e491d11271e4c55217
girma <girmatarekegn.gi@gmail.com>
2023-07-28 20:55:42 +0300

    Add database configeration file

---
+++ backend/config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+
+const connectDb = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI)
+
+        console.log(`Mongoose DB connected: ${conn.connection.host}`.cyan.underline)
+    } catch (error) {
+        console.log(error)
+        process.exit(1)
+    }
+}
+
+module.exports = {
+    connectDb
+}
